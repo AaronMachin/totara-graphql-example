5741baa7e8f3338b38ccc51eb8a82f6b62328984
Aaron Machin <131938187+AaronMachin@users.noreply.github.com>
2025-09-24 21:31:42 +1200

    Update version.php

--- version.php
+++ version.php
@@ -2,6 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026010100;     // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2026010100;        // Requires this Totara version
+// if this doesn't work, change the two versions to be the same as the other plugins in your site
+$plugin->version   = 2024000000;     // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2024000000;        // Requires this Totara version
 $plugin->component = 'local_todo';         // Full name of the plugin (used for diagnostics)
